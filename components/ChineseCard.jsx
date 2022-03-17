286f3932326e3a139ab4ef64563c2c2b18587c45
lim-jiahao <lim.jiahaoo@gmail.com>
2022-03-18 00:58:01 +0800

    add text to speech function

--- components/ChineseCard.jsx
+++ components/ChineseCard.jsx
@@ -1,22 +1,47 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import {
+  View, Text, StyleSheet, TouchableOpacity,
+} from 'react-native';
+import * as Speech from 'expo-speech';
+import { MicrophoneIcon } from 'react-native-heroicons/outline';
 
 const styles = StyleSheet.create({
   listItem: {
     padding: 10,
     marginVertical: 10,
-    backgroundColor: "#ccc",
-    borderColor: "black",
+    backgroundColor: '#ccc',
+    borderColor: 'black',
     borderWidth: 1,
+  },
+  characters: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  chineseText: {
+    marginRight: 5,
   },
 });
 
-const ChineseCard = ({ item }) => (
-  <View style={styles.listItem}>
-    <Text>{item.characters}</Text>
-    <Text>{item.pinyin}</Text>
-    <Text>{item.translation}</Text>
-  </View>
-);
+const ChineseCard = ({ item }) => {
+  const speakText = (text) => {
+    try {
+      Speech.speak(text, { language: 'zh-CN' });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <View style={styles.listItem}>
+      <View style={styles.characters}>
+        <Text style={styles.chineseText}>{item.characters}</Text>
+        <TouchableOpacity onPress={() => speakText(item.characters)}>
+          <MicrophoneIcon color="black" size={16} fill="black" />
+        </TouchableOpacity>
+      </View>
+      <Text>{item.pinyin}</Text>
+      <Text>{item.translation}</Text>
+    </View>
+  ); };
 
 export default ChineseCard;
