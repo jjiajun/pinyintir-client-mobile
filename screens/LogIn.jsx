17db408f95decfc9a9e15c90ac343d862bf98c8f
lim-jiahao <lim.jiahaoo@gmail.com>
2022-03-22 03:09:53 +0800

    add activity spinner to login page while authenticating user credentials

--- screens/LogIn.jsx
+++ screens/LogIn.jsx
@@ -9,6 +9,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Image,
+  ActivityIndicator,
 } from 'react-native';
 import Message from '../components/Message.jsx';
 import Colors from '../constants/colors.js';
@@ -50,42 +51,53 @@ const LogIn = ({ navigation }) => {
   const [password, setPassword] = useState('');
   // State and setter for signup and login message
   const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
   console.log(`${REACT_APP_BACKEND}/login`);
 
-  const loginAttempt = () => {
+  const handleEmailChange = (el) => {
+    setEmail(el);
+    setMessage('');
+  };
+
+  const handlePasswordChange = (el) => {
+    setPassword(el);
+    setMessage('');
+  };
+
+  const loginAttempt = async () => {
     // immediately reject log in if there is a missing field\
     if (!email || !password) {
       setMessage('Please enter an email and password');
       return;
     }
     // wrap email and data in an object for easier manipulation
-    const data = {
-      email,
-      password,
-    };
-    // verify log in. if not verified, send error messages
-    axios
-      .post(`${REACT_APP_BACKEND}/user/login`, data)
-      .then((response) => {
-        // If username or password incorrect, inform player
-        if (response.data === 'The email or password is incorrect') {
-          setMessage('Invalid login. Please try again.');
-        }
-        // If successful, redirect to home page
-        if (response.data.success === true) {
-          const { userId, token } = response.data;
-          console.log('userId: ', userId);
-          console.log('token: ', token);
-          const storeData = async () => {
-            await AsyncStorage.setItem('@sessionToken', token);
-            await AsyncStorage.setItem('@userId', userId);
-          };
-          setEmail('');
-          setPassword('');
-          storeData().then(() => navigation.navigate('Home'));
-        }
-      })
-      .catch((err) => console.log(err));
+    const data = { email, password };
+    try {
+      setLoading(true);
+      // verify log in. if not verified, send error messages
+      const response = await axios.post(`${REACT_APP_BACKEND}/user/login`, data);
+
+      setLoading(false);
+      // If username or password incorrect, inform player
+      if (response.data === 'The email or password is incorrect') {
+        setMessage('Invalid login. Please try again.');
+        return;
+      }
+      // If successful, redirect to home page
+      if (response.data.success === true) {
+        const { userId, token } = response.data;
+        console.log('userId: ', userId);
+        console.log('token: ', token);
+        setEmail('');
+        setPassword('');
+        setMessage('');
+        await AsyncStorage.setItem('@sessionToken', token);
+        await AsyncStorage.setItem('@userId', userId);
+        navigation.navigate('Home');
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -107,7 +119,7 @@ const LogIn = ({ navigation }) => {
             blurOnSubmit
             autoCapitalize="none"
             autoCorrect={false}
-            onChangeText={(el) => setEmail(el)}
+            onChangeText={handleEmailChange}
             value={email}
           />
           <Input
@@ -117,15 +129,19 @@ const LogIn = ({ navigation }) => {
             blurOnSubmit
             autoCapitalize="none"
             autoCorrect={false}
-            onChangeText={(el) => setPassword(el)}
+            onChangeText={handlePasswordChange}
             value={password}
           />
-          <CustomButton
-            style={styles.button}
-            title="Log In"
-            color={Colors.primary}
-            onPress={loginAttempt}
-          />
+          {loading
+            ? (<ActivityIndicator animating={loading} size="large" color="#00ff00" />)
+            : (
+              <CustomButton
+                style={styles.button}
+                title="Log In"
+                color={Colors.primary}
+                onPress={loginAttempt}
+              />
+            )}
           <View>{message !== '' && <Message message={message} />}</View>
         </Card>
       </View>
